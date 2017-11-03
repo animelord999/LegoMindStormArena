8e433b923b3682891862eeb49e5c0bb42f3f910a
NadeemAbu <33332702+NadeemAbu@users.noreply.github.com>
2017-11-03 15:09:39 +1100

    Connect Class

---
+++ LegoMindStormArenaProject/LegoMindStormArenaProject/Connect.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lego.Ev3.Desktop;
+
+namespace LegoMindStormArenaProject
+{ 
+    class Connect
+    {
+
+        public Brick _Brick;
+        public async Task Connecting()
+        { 
+        //_Brick = new Brick(new BluetoothCommunication("COM6"));
+        _Brick = new Brick(new UsbCommunication());
+            _Brick.BrickChanged += _Brick_BrickChanged;
+
+            await _Brick.ConnectAsync();
+        }
+    }
+}
