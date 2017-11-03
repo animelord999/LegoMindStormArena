23eb736f5f10d8b6dde823bdb40174aa6ca67b5e
animelord999 <33332683+animelord999@users.noreply.github.com>
2017-11-03 15:31:18 +1100

    Has Desktop and core now

---
+++ Lego.Ev3.Desktop/Create Brick.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lego.Ev3.Core;
+using Lego.Ev3.Desktop;
+
+namespace Lego.Ev3
+{
+    
+    public class Create_Brick
+    {
+        public float buttonstate = 0;
+        public Brick brick;
+        
+
+        public async Task DoStuff()
+        {
+            brick = new Brick(new UsbCommunication());
+
+            //brick.BrickChanged += Brick_BrickChanged;
+            Console.WriteLine("Connecting...");
+            await brick.ConnectAsync();
+            Console.WriteLine("Connected.");
+        }
+        
+    }
+}
