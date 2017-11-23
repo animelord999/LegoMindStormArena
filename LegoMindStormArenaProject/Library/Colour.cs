38cbc7ab3a825a8bd7215492170e78429e312a04
Bailey Gatherum <1622595@student.swin.edu.au>
2017-11-24 10:55:04 +1100

    Nadeeem's Idea

--- LegoMindStormArenaProject/Library/Colour.cs
+++ LegoMindStormArenaProject/Library/Colour.cs
@@ -13,27 +13,61 @@ namespace Library
 {
     class Colour : Sensor
     {
-        public int sumAverage(params int[] avgDistance)
-        {
-            int result = 0;
+        Brick brick;
 
-            for (int i = 0; i < avgDistance.Length; i++)
-            {
-                result += avgDistance[i];
-            }
+        public double blue { get; private set; }
+        public double black { get; private set; }
+        public double yellow { get; private set; }
+        public double red { get; private set; }
 
-            return result;
-        }
+        /*
+public int sumAverage(params int[] avgDistance)
+{
+int result = 0;
 
+for (int i = 0; i < avgDistance.Length; i++)
+{
+result += avgDistance[i];
+}
+
+return result;
+}*/
+        /*
         public int black = 1;
         public int blue = 2;
         public int yellow = 4;
         public int red = 5;
-
-        public int result { get; private set; }
+        */
+        //public int result { get; private set; }
 
         public double Seekcolour(object sender, BrickChangedEventArgs e)
         {
+
+            getColour(brick);
+            float colourv = getColour(brick);
+
+            if (colourv == 0)
+            {
+                return black;
+            }
+            else if (colourv == 3)
+            {
+                return blue;
+            }
+            else if (colourv == 1)
+            {
+                return yellow;
+            }
+            else if (colourv == 5)
+            {
+                return red;
+            }
+            else
+            {
+                return -1;
+            }
+
+            /*
             switch (result)
             {
                 case 1:
@@ -51,6 +85,7 @@ namespace Library
                 default:
                     return -1;
             }
+            */
         }
     }
 }
